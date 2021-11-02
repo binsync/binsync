dc14ac4900a4524e9548d8085ebafd104a467d19
frqmod <me@frqmod.dev>
2021-11-02 14:35:59 -0700

    small bugfixes, ui improvements

--- plugins/angr_binsync/controller.py
+++ plugins/angr_binsync/controller.py
@@ -35,10 +35,10 @@ class AngrBinSyncController(BinSyncController):
         except NotImplementedError:
             return None
 
-        if func is None:
+        if func is None or func.am_obj is None:
             return None
 
-        return func.addr
+        return func.name, func.addr
 
     #
     # Display Fillers
