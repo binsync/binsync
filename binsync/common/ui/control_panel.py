865583a5e93608b33c9373ad37291594e57159fa
frqmod <71472767+frqmod@users.noreply.github.com>
2021-11-07 22:16:58 +0000

    fix syntax error

--- binsync/common/ui/control_panel.py
+++ binsync/common/ui/control_panel.py
@@ -93,7 +93,7 @@ class ControlPanel(QWidget):
     def _update_ctx(self):
         if isinstance(self.controller.last_ctx, binsync.data.Function):
             self._ctx_table.update_table(new_ctx=self.controller.last_ctx.addr)
-            symbol = (self.controller.last_ctx_name[:12]+"..") if len(self.controller.last_ctx.name) > 12 else self.controller.last_ctx.name
+            symbol = (self.controller.last_ctx.name[:12]+"..") if len(self.controller.last_ctx.name) > 12 else self.controller.last_ctx.name
             self._status_bar.showMessage(f"{symbol}@{hex(self.controller.last_ctx.addr)}")
         else:
             return
