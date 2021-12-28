39a53a84c640314adbf50e612177c4a56c43542c
Zion <zbasque@asu.edu>
2021-12-27 18:58:40 -0700

    fix a crash when making a stack var in a non-existent function (#83)
    * fix a crash when making a stack var in a non-existent function

--- binsync/data/artifact.py
+++ binsync/data/artifact.py
@@ -1,8 +1,8 @@
 import toml
+from typing import Dict
 
 
 class Artifact:
-
     __slots__ = (
         "last_change"
     )
@@ -10,16 +10,35 @@ class Artifact:
     def __init__(self, last_change=None):
         self.last_change = None
 
-    def __getstate__(self):
+    def __getstate__(self) -> Dict:
+        """
+        Returns a dict of all the properties of the artifact. With the key as their name
+        and the value as their value.
+
+        @return:
+        """
         return dict(
             (k, getattr(self, k)) for k in self.__slots__
         )
 
     def __setstate__(self, state):
+        """
+        Sets all the properties of the artifact given a dict of keys and values.
+        Note: the values can also be dicts.
+
+        @param state: Dict
+        @return:
+        """
         for k in self.__slots__:
             setattr(self, k, state.get(k, None))
 
     def __eq__(self, other):
+        """
+        Like a normal == override but we always ignore last_push.
+
+        @param other: Another Artifact
+        @return:
+        """
         if not isinstance(other, self.__class__):
             return False
 
@@ -32,9 +51,21 @@ class Artifact:
 
         return True
 
-    def dump(self):
+    def dump(self) -> str:
+        """
+        Returns a string in TOML form of the properties of the current artifact. Best used to
+        write directly into a file and save as a .toml file.
+
+        @return:
+        """
         return toml.dumps(self.__getstate__())
 
     @classmethod
     def parse(cls, s):
+        """
+        Parses a TOML form string.
+
+        @param s:
+        @return:
+        """
         raise NotImplementedError()
--- binsync/data/func.py
+++ binsync/data/func.py
@@ -2,7 +2,7 @@ import os
 import time
 
 import toml
-import typing
+from typing import Dict, Optional
 
 from collections import defaultdict
 
@@ -53,7 +53,7 @@ class FunctionHeader(Artifact):
         self.addr = addr
         self.comment = comment
         self.ret_type = ret_type
-        self.args = args
+        self.args = args or {}
 
     def __getstate__(self):
         args = {str(idx): arg.__getstate__() for idx, arg in self.args.items()} if self.args else {}
@@ -63,9 +63,18 @@ class FunctionHeader(Artifact):
             "name": self.name,
             "addr": self.addr,
             "comment": self.comment,
-            "ret_type_str": self.ret_type,
+            "ret_type": self.ret_type,
             "args": args if len(args) > 0 else None,
         }
+
+    def __setstate__(self, state):
+        self.last_change = state.get("last_change", None)
+        self.name = state.get("name", None)
+        self.addr = state["addr"]
+        self.comment = state.get("comment", None)
+        self.ret_type = state.get("ret_type", None)
+        args = state.get("args", {})
+        self.args = {int(idx, 16): StackVariable.parse(toml.dumps(arg)) for idx, arg in args.items()}
 
     @classmethod
     def parse(cls, s):
@@ -102,15 +111,14 @@ class Function(Artifact):
 
     def __init__(self, addr, header=None, stack_vars=None, last_change=None):
         super(Function, self).__init__(last_change=last_change)
-
-        self.addr = addr
-        self.header = header
-        self.stack_vars: typing.Dict[int, StackVariable] = stack_vars
+        self.addr: int = addr
+        self.header: Optional[FunctionHeader] = header
+        self.stack_vars: Dict[int, StackVariable] = stack_vars or {}
 
     def __getstate__(self):
         header = self.header.__getstate__() if self.header else None
         stack_vars = {"%x" % offset: stack_var.__getstate__() for offset, stack_var in self.stack_vars.items()} if \
-            self.stack_vars else None
+            self.stack_vars else {}
 
         return {
             "metadata": {
@@ -118,30 +126,23 @@ class Function(Artifact):
                 "last_change": self.last_change
             },
             "header": header,
-            "stack_vars": stack_vars
+            "stack_vars": stack_vars if len(stack_vars) > 0 else None
         }
 
     def __setstate__(self, state):
         if not isinstance(state["metadata"]["addr"], int):
             raise TypeError("Unsupported type %s for addr." % type(state["metadata"]["addr"]))
 
-        metadata, header, stack_vars = state["metadata"], state.get("header", None), state.get("stack_vars", None)
+        metadata, header, stack_vars = state["metadata"], state.get("header", None), state.get("stack_vars", {})
 
         self.addr = metadata["addr"]
         self.last_change = metadata.get("last_change", None)
 
-        self.header = FunctionHeader.parse(toml.dumps(header))
+        self.header = FunctionHeader.parse(toml.dumps(header)) if header else None
 
         self.stack_vars = {
             int(off, 16): StackVariable.parse(toml.dumps(stack_var)) for off, stack_var in stack_vars.items()
         } if stack_vars else {}
-
-    @property
-    def name(self):
-        return self.header.name if self.header else None
-
-    def set_stack_var(self, name, off: int, off_type: int, size: int, type_str, last_change):
-        self.stack_vars[off] = StackVariable(off, off_type, name, type_str, size, self.addr, last_change=last_change)
 
     @classmethod
     def parse(cls, s):
@@ -154,3 +155,36 @@ class Function(Artifact):
         f = Function(None)
         f.__setstate__(func_toml)
         return f
+
+    #
+    # Property Shortcuts (Alias)
+    #
+
+    @property
+    def name(self):
+        return self.header.name if self.header else None
+
+    @name.setter
+    def name(self, value):
+        # create a header if one does not exist for this function
+        if not self.header:
+            self.header = FunctionHeader(None, self.addr)
+        self.header.name = value
+
+    @property
+    def args(self):
+        return self.header.args
+
+    @property
+    def comment(self):
+        return self.header.comment if self.header else None
+
+    @comment.setter
+    def comment(self, value):
+        # create a header if one does not exist for this function
+        if not self.header:
+            self.header = FunctionHeader(None, self.addr)
+        self.header.name = value
+
+    def set_stack_var(self, name, off: int, off_type: int, size: int, type_str, last_change):
+        self.stack_vars[off] = StackVariable(off, off_type, name, type_str, size, self.addr, last_change=last_change)
