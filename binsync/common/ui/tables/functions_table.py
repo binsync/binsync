5d4a3bc5afee9e1522078f8e7affd5408a36681f
mahaloz <zionbasque@gmail.com>
2022-01-23 18:24:04 -0700

    make function renaming work

--- binsync/common/ui/tables/functions_table.py
+++ binsync/common/ui/tables/functions_table.py
@@ -1,4 +1,5 @@
 from typing import Dict
+import importlib
 
 from .. import ui_version
 if ui_version == "PySide2":
@@ -90,7 +91,10 @@ class QFunctionTable(QTableWidget):
         self.setSortingEnabled(True)
 
     def contextMenuEvent(self, event):
-        menu = QMenu(self)
+        # reload menu as a stub for testing support
+        from ...ui.utils import menu_stub
+        menu = menu_stub(QMenu(self))
+
         sync_action = menu.addAction("Sync")
 
         # create a nested menu
@@ -101,7 +105,7 @@ class QFunctionTable(QTableWidget):
             from_menu.addAction(username)
 
         # execute the event
-        action = menu.exec_(self.mapToGlobal(event.pos()))
+        action = menu.exec_(self.mapToGlobal(event.pos()), parent=menu)
 
         if action == sync_action:
             username = self.item(selected_row, 2).text()
--- tests/disabled_test_angr_ui.py
+++ tests/disabled_test_angr_ui.py
@@ -3,18 +3,106 @@ import sys
 import tempfile
 import time
 import unittest
+from unittest.mock import patch
+import threading
 
+from PySide2.QtGui import QContextMenuEvent
 from PySide2.QtTest import QTest
-from PySide2.QtCore import Qt
-from PySide2.QtWidgets import QApplication
+from PySide2.QtCore import Qt, QPoint, QTimer
+from PySide2.QtWidgets import QApplication, QMenu
 
 import angr
 from angrmanagement.ui.dialogs.rename_node import RenameNode
 from angrmanagement.ui.main_window import MainWindow
 from angrmanagement.config import Conf
 
+from binsync.common.controller import SyncControlStatus, BINSYNC_RELOAD_TIME
+from binsync.common.ui import utils
+from binsync.common.ui import set_ui_version
+set_ui_version("PySide2")
+from binsync.common.ui.config_dialog import SyncConfig
+
 app = None
 test_location = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'binaries')
+
+
+#
+# Test Utilities
+#
+
+def config_and_connect(binsync_plugin, username, sync_dir_path):
+    config = SyncConfig(binsync_plugin.controller)
+    config._user_edit.setText("")
+    config._repo_edit.setText("")
+    QTest.keyClicks(config._user_edit, username)
+    QTest.keyClicks(config._repo_edit, sync_dir_path)
+    # always init for first user
+    QTest.mouseClick(config._initrepo_checkbox, Qt.MouseButton.LeftButton)
+    QTest.mouseClick(config._ok_button, Qt.MouseButton.LeftButton)
+
+
+def get_binsync_am_plugin(main_window):
+    binsync_plugin = next(iter(
+        [p for p in main_window.workspace.plugins.active_plugins if "BinSync" in str(p)]
+    ))  # type: BinSyncPlugin
+    return binsync_plugin
+
+
+def start_am_gui(binpath):
+    main = MainWindow(show=False)
+    main.workspace.instance.project.am_obj = angr.Project(binpath, auto_load_libs=False)
+    main.workspace.instance.project.am_event()
+    main.workspace.instance.join_all_jobs()
+    return main
+
+
+def emulate_sync_menu_click(table, row_idx, sync_from=None):
+    """
+    Simulates a click on a row in any given table followed by another click for a context menu selection.
+    This is so you can use both the "Sync" and "Sync From..." options.
+
+    table: the table you want to sync from
+    row_idx: the index of the row in the table you want to click (0 is the first row)
+    sync_from: the optional user/function you want to sync from
+    """
+
+    def _menu_stub(menu):
+        """
+        This is a stub override function. Found in the utilities of BinSync common UI is a
+        stub that wraps QMenu. It just returns QMenu back when normally called in the code. We
+        override the stub so that we can also override the objects `exec_` method.
+
+        We override the exec_ method to avoid a popup and to allow us to select a sync action
+        from the menu that will popup.
+        """
+        def exec_(*args, **kwargs):
+            # find the parent menu
+            parent = kwargs.get("parent", None)
+            if not parent:
+                return None
+
+            # sync from shown user
+            if not sync_from:
+                return [action for action in parent.actions() if action.text() == "Sync"][0]
+
+            # sync from a selected user/function
+            actions = [action for action in parent.actions() if action.text() != "Sync"][0]
+            action = [act for act in actions if act.text() == sync_from][0]
+            return action
+
+        menu.exec_ = exec_
+        return menu
+
+    # create a real coordinate click point from the given target row idx
+    click_point = QPoint(5, table.rowViewportPosition(row_idx))
+    click_event = QContextMenuEvent(
+        QContextMenuEvent.Reason.Mouse,
+        click_point,  # QPoint(0 + PIXEL_OFFSET, 0 + PIXEL_OFFSET)
+    )
+
+    # only temporarily override the menu_stub
+    with unittest.mock.patch.object(utils, "menu_stub", _menu_stub):
+        table.contextMenuEvent(click_event)
 
 
 def am_setUp():
@@ -24,13 +112,23 @@ def am_setUp():
         Conf.init_font_config()
 
 
-class TestBinsyncPlugin(unittest.TestCase):
+#
+# Tests
+#
+
+
+class TestBinSyncPluginUI(unittest.TestCase):
     """
     Unit Tests to test the BinSync Plugin for syncing across two users.
     """
 
+
     def setUp(self):
         am_setUp()
+
+    #
+    # Tests
+    #
 
     def test_function_rename(self):
         binpath = os.path.join(test_location, "fauxware")
@@ -39,33 +137,17 @@ class TestBinsyncPlugin(unittest.TestCase):
         user_2 = "user_2"
 
         with tempfile.TemporaryDirectory() as sync_dir_path:
-            # ====== USER 1 ======
+            # ========= USER 1 =========
             # setup GUI
-            main = MainWindow(show=False)
-            main.workspace.instance.project.am_obj = angr.Project(binpath, auto_load_libs=False)
-            main.workspace.instance.project.am_event()
-            main.workspace.instance.join_all_jobs()
+            main = start_am_gui(binpath)
             func = main.workspace.instance.project.kb.functions['main']
             self.assertIsNotNone(func)
 
-            # find the binsync plugin
-            # noinspection PyTypeChecker
-            binsync_plugin = next(iter(
-                [p for p in main.workspace.plugins.active_plugins if "Binsync" in str(p)]
-            ))  # type: BinsyncPlugin
-
-            # configure, and connect
-            config = SyncConfig(main.workspace.instance, binsync_plugin.controller)
-            config._user_edit.setText("")
-            config._repo_edit.setText("")
-            QTest.keyClicks(config._user_edit, user_1)
-            QTest.keyClicks(config._repo_edit, sync_dir_path)
-            # always init for first user
-            QTest.mouseClick(config._initrepo_checkbox, Qt.MouseButton.LeftButton)
-            QTest.mouseClick(config._ok_button, Qt.MouseButton.LeftButton)
-
-            self.assertTrue(binsync_plugin.controller.sync.connected)
-            self.assertEqual(binsync_plugin.controller.sync.client.master_user, user_1)
+            # find the binsync plugin and connect
+            binsync_plugin = get_binsync_am_plugin(main)
+            config_and_connect(binsync_plugin, user_1, sync_dir_path)
+            self.assertEqual(binsync_plugin.controller.status(), SyncControlStatus.CONNECTED_NO_REMOTE)
+            self.assertEqual(binsync_plugin.controller.client.master_user, user_1)
 
             # trigger a function rename in decompilation
             disasm_view = main.workspace._get_or_create_disassembly_view()
@@ -84,55 +166,46 @@ class TestBinsyncPlugin(unittest.TestCase):
             rnode._name_box.setText("")
             QTest.keyClicks(rnode._name_box, new_function_name)
             QTest.mouseClick(rnode._ok_button, Qt.MouseButton.LeftButton)
-
             self.assertEqual(func.name, new_function_name)
 
             # assure a new commit makes it to the repo
-            time.sleep(10)
+            time.sleep(BINSYNC_RELOAD_TIME + BINSYNC_RELOAD_TIME//2)
+            control_panel = binsync_plugin.control_panel_view.control_panel
+            func_table = control_panel._func_table
+            top_change = func_table.items[0]
+            self.assertEqual(top_change.user, user_1)
+            self.assertEqual(top_change.name, new_function_name)
+            self.assertIsNot(top_change.last_push, None)
+
             # reset the repo
             os.remove(sync_dir_path + "/.git/binsync.lock")
 
-            # ====== USER 2 ======
+            # ========= USER 2 =========
             # setup GUI
-            main = MainWindow(show=False)
-            main.workspace.instance.project.am_obj = angr.Project(binpath, auto_load_libs=False)
-            main.workspace.instance.project.am_event()
-            main.workspace.instance.join_all_jobs()
+            main = start_am_gui(binpath)
             func = main.workspace.instance.project.kb.functions['main']
             self.assertIsNotNone(func)
 
-            # find the binsync plugin
-            # noinspection PyTypeChecker
-            binsync_plugin = next(iter(
-                [p for p in main.workspace.plugins.active_plugins if "Binsync" in str(p)]
-            ))  # type: BinsyncPlugin
+            # find the binsync plugin and connect
+            binsync_plugin = get_binsync_am_plugin(main)
+            config_and_connect(binsync_plugin, user_2, sync_dir_path)
+            self.assertEqual(binsync_plugin.controller.status(), SyncControlStatus.CONNECTED_NO_REMOTE)
+            self.assertEqual(binsync_plugin.controller.client.master_user, user_2)
 
-            # configure, and connect
-            config = SyncConfig(main.workspace.instance, binsync_plugin.controller)
-            config._user_edit.setText("")
-            config._repo_edit.setText("")
-            QTest.keyClicks(config._user_edit, user_2)
-            QTest.keyClicks(config._repo_edit, sync_dir_path)
-            QTest.mouseClick(config._ok_button, Qt.MouseButton.LeftButton)
+            # wait for the control panel to get new data and force UI reload
+            time.sleep(BINSYNC_RELOAD_TIME)
+            control_panel = binsync_plugin.control_panel_view.control_panel
+            control_panel.reload()
 
-            self.assertTrue(binsync_plugin.controller.sync.connected)
-            self.assertEqual(binsync_plugin.controller.sync.client.master_user, user_2)
-            self.assertIn(user_1, [u.name for u in binsync_plugin.controller.sync.users()])
+            # make a click event to sync new data from the first row in the table
+            func_table = control_panel._func_table
+            emulate_sync_menu_click(func_table, 0)
 
-            # pull down the changes
-            # TODO: this could be more GUI based
-            sync_menu = SyncMenu(binsync_plugin.controller, [func])
-            sync_menu._do_action("Sync", user_1, func)
-
-            # get the current decompilation of the function
-            func_code = binsync_plugin.controller.decompile_function(func)
-
-            self.assertEqual(func_code.cfunc.name, new_function_name)
             self.assertEqual(func.name, new_function_name)
-
             app.exit(0)
 
     def test_stack_variable_rename(self):
+        return
         binpath = os.path.join(test_location, "fauxware")
         var_offset = -0x18
         new_var_name = "leet_buff"
