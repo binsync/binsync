f99046192b48a47b8781cb51405074c87bb8f537
Zion <zionbasque@gmail.com>
2022-04-16 20:00:06 -0700

    Update __init__.py

--- plugins/ida_binsync/ida_binsync/__init__.py
+++ plugins/ida_binsync/ida_binsync/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-VERSION = "v2.1.0"
+VERSION = "v2.2.0"
 PLUGIN_DIR = os.path.dirname(os.path.realpath(__file__))
 IDA_DIR = os.path.abspath(os.path.join(PLUGIN_DIR, "..", ".."))
 UI_DIR = os.path.join(PLUGIN_DIR, "ui")
