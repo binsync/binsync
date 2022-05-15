1f1eafff878273b9abb288ed4c047c0b7dcfeb89
mahaloz <zionbasque@gmail.com>
2022-05-15 16:30:48 -0700

    make the UI boxier

--- binsync/common/ui/panel_tabs/util_panel.py
+++ binsync/common/ui/panel_tabs/util_panel.py
@@ -51,12 +51,29 @@ class QUtilPanel(QWidget):
 
 
     def _init_widgets(self):
-        info_label = QLabel("Hovering over each option provides further explanation.")
-        info_label.setAlignment(Qt.AlignCenter)
-        info_label.setWordWrap(True)
+
+        #
+        # Developer Options Group
+        #
+
+        dev_options_group = QGroupBox()
+        dev_options_layout = QVBoxLayout()
+        dev_options_group.setTitle("Developer Options")
+        dev_options_group.setLayout(dev_options_layout)
+
         self._debug_log_toggle = QCheckBox("Toggle Debug Logging")
         self._debug_log_toggle.setToolTip("Toggles the logging of events BinSync developers care about.")
         self._debug_log_toggle.stateChanged.connect(self._handle_debug_toggle)
+        dev_options_layout.addWidget(self._debug_log_toggle)
+
+        #
+        # Sync Options Group
+        #
+
+        sync_options_group = QGroupBox()
+        sync_options_layout = QVBoxLayout()
+        sync_options_group.setTitle("Sync Options")
+        sync_options_group.setLayout(sync_options_layout)
 
         self._sync_level_label = QLabel("Sync Level")
         self._sync_level_label.setToolTip(
@@ -73,23 +90,28 @@ class QUtilPanel(QWidget):
         self._sync_level_combobox = QComboBox()
         self._sync_level_combobox.addItems(["Non-Conflicting", "Overwrite", "Merge"])
         self._sync_level_combobox.currentIndexChanged.connect(self._handle_sync_level_change)
-        sync_level_group = QGroupBox()
-        sync_level_group.setLayout(QHBoxLayout())
-        sync_level_group.layout().setContentsMargins(0, 0, 0, 0)
-        sync_level_group.layout().addWidget(self._sync_level_label)
-        sync_level_group.layout().addWidget(self._sync_level_combobox)
 
-        self._magic_sync_button = QPushButton("Magic Sync")
+        sync_level_layout = QHBoxLayout()
+        #sync_level_group.layout().setContentsMargins(0, 0, 0, 0)
+        sync_level_layout.addWidget(self._sync_level_label)
+        sync_level_layout.addWidget(self._sync_level_combobox)
+
+        self._magic_sync_button = QPushButton("Initiate Magic Sync")
         self._magic_sync_button.clicked.connect(self._handle_magic_sync_button)
         self._magic_sync_button.setToolTip("Performs a best effort merge of all existing user data to your state, but won't affect your existing state (this uses a non-conflicting merge).")
+
+        sync_options_layout.addLayout(sync_level_layout)
+        sync_options_group.layout().addWidget(self._magic_sync_button)
+
+        #
+        # Final Layout
+        #
 
         main_layout = QVBoxLayout()
         main_layout.setContentsMargins(10, 20, 10, 20)
         main_layout.setSpacing(18)
         main_layout.setAlignment(Qt.AlignTop)
-        main_layout.addWidget(info_label)
-        main_layout.addWidget(sync_level_group)
-        main_layout.addWidget(self._debug_log_toggle)
-        main_layout.addWidget(self._magic_sync_button)
+        main_layout.addWidget(sync_options_group)
+        main_layout.addWidget(dev_options_group)
         self.setLayout(main_layout)
 
