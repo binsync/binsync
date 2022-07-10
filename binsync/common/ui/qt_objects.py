27a10ce3473373dc60f2da592f5053ffaeacefaa
Dðnkèy <63219051+realdonkey@users.noreply.github.com>
2022-07-10 13:42:15 -0700

    add more imports for force_push

--- binsync/common/ui/qt_objects.py
+++ binsync/common/ui/qt_objects.py
@@ -1,7 +1,7 @@
 from binsync.common.ui.version import ui_version
 
 if ui_version == "PySide2":
-    from PySide2.QtCore import QDir, Qt, Signal, QEvent, QPersistentModelIndex
+    from PySide2.QtCore import QDir, Qt, Signal, QEvent, QPersistentModelIndex, QAbstractTableModel, QModelIndex, QSortFilterProxyModel
     from PySide2.QtWidgets import (
         QAbstractItemView,
         QCheckBox,
@@ -25,11 +25,12 @@ if ui_version == "PySide2":
         QWidget,
         QDialogButtonBox,
         QAction,
-        QStyledItemDelegate
+        QStyledItemDelegate,
+        QTableView
     )
-    from PySide2.QtGui import QFontDatabase
+    from PySide2.QtGui import QFontDatabase, QColor, QFocusEvent, QKeyEvent
 elif ui_version == "PySide6":
-    from PySide6.QtCore import QDir, Qt, Signal, QEvent, QPersistentModelIndex
+    from PySide6.QtCore import QDir, Qt, Signal, QEvent, QPersistentModelIndex, QAbstractTableModel, QModelIndex, QSortFilterProxyModel
     from PySide6.QtWidgets import (
         QAbstractItemView,
         QCheckBox,
@@ -53,13 +54,14 @@ elif ui_version == "PySide6":
         QWidget,
         QDialogButtonBox,
         QAction,
-        QStyledItemDelegate
+        QStyledItemDelegate,
+        QTableView
     )
-    from PySide6.QtGui import QFontDatabase
+    from PySide6.QtGui import QFontDatabase, QColor, QFocusEvent, QKeyEvent
 else:
     from PyQt5.QtCore import QDir, Qt, QEvent
     from PyQt5.QtCore import pyqtSignal as Signal
-    from PyQt5.QtCore import QPersistentModelIndex
+    from PyQt5.QtCore import QPersistentModelIndex, QAbstractTableModel, QModelIndex, QSortFilterProxyModel
     from PyQt5.QtWidgets import (
         QAbstractItemView,
         QCheckBox,
@@ -83,6 +85,7 @@ else:
         QWidget,
         QDialogButtonBox,
         QAction,
-        QStyledItemDelegate
+        QStyledItemDelegate,
+        QTableView
     )
-    from PyQt5.QtGui import QFontDatabase
+    from PyQt5.QtGui import QFontDatabase, QColor, QFocusEvent, QKeyEvent
