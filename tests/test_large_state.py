bb9d8cdacb0d61e78ca8bd46bdebb1dfe7ebc3de
frqmod <me@frqmod.dev>
2022-08-09 11:33:01 -0700

    init commit for large test case

---
+++ tests/test_large_state.py
@@ -0,0 +1,42 @@
+import os
+import sys
+import tempfile
+
+import unittest
+
+import binsync
+
+class TestClient(unittest.TestCase):
+    def test_client_creation(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            client = binsync.Client("user0", tmpdir, "fake_hash", init_repo=True)
+            self.assertTrue(os.path.isdir(os.path.join(tmpdir, ".git")))
+
+    def test_client_state(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            client = binsync.Client("user0", tmpdir, "fake_hash", init_repo=True)
+
+            state = client.get_state()
+            self.assertEqual(state.user, "user0")
+            # after create, state is dirty
+            self.assertTrue(state.dirty)
+
+            func_header = binsync.data.FunctionHeader("some_name", 0x400080)
+            state.set_function_header(func_header)
+            # it should be dirty still (more edits)
+            self.assertTrue(state.dirty)
+
+            # commit changes so we clean it!
+            client.commit_state(state)
+            self.assertFalse(state.dirty)
+
+            state = client.get_state(user="user0")
+            self.assertTrue(len(state.functions), 1)
+            self.assertTrue(state.functions[0x400080].header, func_header)
+
+            # git is still running at least on windows
+            client.close()
+
+
+if __name__ == "__main__":
+    unittest.main(argv=sys.argv)
