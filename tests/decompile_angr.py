5e00a1dd7457d8a5272821bc55dc7166948a866d
Han Dai <github@daihan.me>
2022-08-09 22:24:29 +0000

    md5 and filename

--- tests/decompile_angr.py
+++ tests/decompile_angr.py
@@ -2,27 +2,33 @@ import angr
 from collections import defaultdict
 import json
 import sys
+import subprocess
+import os
 
 
 def parse_binary(binary_path, output_path):
     p = angr.Project(binary_path, load_options={"auto_load_libs": False})
     cfg = p.analyses.CFG(data_references=True, normalize=True)
     p.analyses[angr.analyses.CompleteCallingConventionsAnalysis].prep()(recover_variables=True, analyze_callsites=True)
-    
-    func_addrs_variables = defaultdict(dict)
+
+    binary_info = {}
+    binary_info["binary"] = os.path.basename(binary_path)
+    binary_info["md5"] = subprocess.check_output(["md5sum", binary_path]).decode().split()[0]
+    binary_info["functions"] = defaultdict(dict)
+
     for addr, func in cfg.functions.items():
         try:
             if func.is_plt or func.is_simprocedure or func.alignment:
                 continue
-            func_addrs_variables[addr]['name'] = func.name
-            func_addrs_variables[addr]['variables'] = []
-            func_addrs_variables[addr]['arguments'] = []
+            binary_info["functions"][addr]['name'] = func.name
+            binary_info["functions"][addr]['variables'] = []
+            binary_info["functions"][addr]['arguments'] = []
             decomp = p.analyses.Decompiler(func, cfg=cfg.model)
             if not decomp.codegen:
                 continue
-            func_addrs_variables[addr]['return_type'] = decomp.codegen.cfunc.functy.returnty.c_repr()
+            binary_info["functions"][addr]['return_type'] = decomp.codegen.cfunc.functy.returnty.c_repr()
             for idx, (arg_type, arg_cvar) in enumerate(zip(decomp.codegen.cfunc.functy.args, decomp.codegen.cfunc.arg_list)):
-                func_addrs_variables[addr]['arguments'].append({"idx": idx, "name": arg_cvar.c_repr(), "type": arg_type.c_repr(), "size": arg_type.size // 8})
+                binary_info["functions"][addr]['arguments'].append({"idx": idx, "name": arg_cvar.c_repr(), "type": arg_type.c_repr(), "size": arg_type.size // 8})
             for i in decomp.codegen.cfunc.variables_in_use:
                 var = decomp.codegen.cfunc.variable_manager.unified_variable(i)
                 var_info = {}
@@ -31,13 +37,13 @@ def parse_binary(binary_path, output_path):
                 if hasattr(var, 'offset'):
                     var_info['offset'] = var.offset
                 if var_info:
-                    func_addrs_variables[addr]['variables'].append(var_info)
+                    binary_info["functions"][addr]['variables'].append(var_info)
         except Exception as e:
             print(f"Error! in {hex(addr)}: {e}")
             continue
 
     with open(output_path, 'w') as f:
-        json.dump(dict(func_addrs_variables), f)
+        json.dump(dict(binary_info), f)
 
 if __name__ == "__main__":
     binary_path = sys.argv[1]
