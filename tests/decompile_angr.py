23e77f366fc89a78636b2916f84bfc4b722ddfc6
Han Dai <github@daihan.me>
2022-08-10 19:32:26 +0000

    bug fix & only catch decompiler exceptions

--- tests/decompile_angr.py
+++ tests/decompile_angr.py
@@ -2,7 +2,7 @@ import angr
 import sys
 import subprocess
 import os
-from angr.knowledge_plugins.cfg.memory_data import MemoryDataSort
+import toml
 
 
 def parse_binary(binary_path, output_path):
@@ -19,31 +19,34 @@ def parse_binary(binary_path, output_path):
 
     # functions
     for addr, func in cfg.functions.items():
-        try:
-            if func.is_plt or func.is_simprocedure or func.alignment:
-                continue
-            function = {"name": func.name, "variables": [], "arguments": []}
-            decomp = p.analyses.Decompiler(func, cfg=cfg.model)
-            if not decomp.codegen:
-                continue
-            for extern in decomp.codegen.cexterns:
-                binary_info["globals"][str(extern.variable.addr)] = {"name": extern.variable.name, "size": extern.variable_type.size // 8, "type": extern.variable_type.c_repr()}
-            function['return_type'] = decomp.codegen.cfunc.functy.returnty.c_repr()
-            for idx, (arg_type, arg_cvar) in enumerate(zip(decomp.codegen.cfunc.functy.args, decomp.codegen.cfunc.arg_list)):
-                function['arguments'].append({"idx": idx, "name": arg_cvar.c_repr(), "type": arg_type.c_repr(), "size": arg_type.size // 8})
-            for i in decomp.codegen.cfunc.variables_in_use:
-                var = decomp.codegen.cfunc.variable_manager.unified_variable(i)
-                var_info = {}
-                if hasattr(var, 'name'):
-                    var_info['name'] = var.name
-                if hasattr(var, 'offset'):
-                    var_info['offset'] = var.offset
-                if var_info:
-                    function['variables'].append(var_info)
-            binary_info["functions"][str(addr)] = function
-        except Exception as e:
-            print(f"Error! in {hex(addr)}: {e}")
+        if func.is_plt or func.is_simprocedure or func.alignment:
             continue
+        function = {"name": func.name, "variables": [], "arguments": []}
+        try:
+            decomp = p.analyses.Decompiler(func, cfg=cfg.model)
+        except Exception as e:
+            print(f"Decompilation Error @ {hex(addr)}: {e}")
+            continue
+        if not decomp.codegen:
+            continue
+        for extern in decomp.codegen.cexterns:
+            if extern.variable_type:
+                binary_info["globals"][str(extern.variable.addr)] = {"name": extern.variable.name, "size": extern.variable_type.size // 8, "type": extern.variable_type.c_repr()}
+            else:
+                binary_info["globals"][str(extern.variable.addr)] = {"name": extern.variable.name}
+        function['return_type'] = decomp.codegen.cfunc.functy.returnty.c_repr()
+        for idx, (arg_type, arg_cvar) in enumerate(zip(decomp.codegen.cfunc.functy.args, decomp.codegen.cfunc.arg_list)):
+            function['arguments'].append({"idx": idx, "name": arg_cvar.c_repr(), "type": arg_type.c_repr(), "size": arg_type.size // 8})
+        for i in decomp.codegen.cfunc.variables_in_use:
+            var = decomp.codegen.cfunc.variable_manager.unified_variable(i)
+            var_info = {}
+            if hasattr(var, 'name'):
+                var_info['name'] = var.name
+            if hasattr(var, 'offset'):
+                var_info['offset'] = var.offset
+            if var_info:
+                function['variables'].append(var_info)
+        binary_info["functions"][str(addr)] = function
 
     with open(output_path, 'w') as f:
         toml.dump(binary_info, f)
