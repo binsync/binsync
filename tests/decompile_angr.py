2f5d5cc011d9460132f23005a4a67588379397c2
Han Dai <github@daihan.me>
2022-08-10 00:19:12 +0000

    global var

--- tests/decompile_angr.py
+++ tests/decompile_angr.py
@@ -4,6 +4,7 @@ import json
 import sys
 import subprocess
 import os
+from angr.knowledge_plugins.cfg.memory_data import MemoryDataSort
 
 
 def parse_binary(binary_path, output_path):
@@ -11,11 +12,14 @@ def parse_binary(binary_path, output_path):
     cfg = p.analyses.CFG(data_references=True, normalize=True)
     p.analyses[angr.analyses.CompleteCallingConventionsAnalysis].prep()(recover_variables=True, analyze_callsites=True)
 
+    # binary info
     binary_info = {}
     binary_info["binary"] = os.path.basename(binary_path)
     binary_info["md5"] = subprocess.check_output(["md5sum", binary_path]).decode().split()[0]
     binary_info["functions"] = defaultdict(dict)
+    binary_info["globals"] = defaultdict(dict)
 
+    # functions
     for addr, func in cfg.functions.items():
         try:
             if func.is_plt or func.is_simprocedure or func.alignment:
@@ -26,6 +30,8 @@ def parse_binary(binary_path, output_path):
             decomp = p.analyses.Decompiler(func, cfg=cfg.model)
             if not decomp.codegen:
                 continue
+            for extern in decomp.codegen.cexterns:
+                binary_info["globals"][extern.variable.addr] = {"name": extern.variable.name, "size": extern.variable_type.size // 8, "type": extern.variable_type.c_repr()}
             binary_info["functions"][addr]['return_type'] = decomp.codegen.cfunc.functy.returnty.c_repr()
             for idx, (arg_type, arg_cvar) in enumerate(zip(decomp.codegen.cfunc.functy.args, decomp.codegen.cfunc.arg_list)):
                 binary_info["functions"][addr]['arguments'].append({"idx": idx, "name": arg_cvar.c_repr(), "type": arg_type.c_repr(), "size": arg_type.size // 8})
