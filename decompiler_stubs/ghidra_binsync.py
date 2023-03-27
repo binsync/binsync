e64b680d53010ba9e471d0d681b0d9cbb15b636a
mahaloz <zionbasque@gmail.com>
2023-03-26 23:18:54 -0700

    Try-catch all stubs

--- decompiler_stubs/ghidra_binsync.py
+++ decompiler_stubs/ghidra_binsync.py
@@ -1,1 +1,4 @@
-from binsync.decompilers.ghidra.server import start
+try:
+    from binsync.decompilers.ghidra.server import start
+except ImportError:
+    print("[!] BinSync is not installed, please `pip install binsync` for THIS python interpreter")
--- decompiler_stubs/ida_binsync.py
+++ decompiler_stubs/ida_binsync.py
@@ -1,3 +1,8 @@
 def PLUGIN_ENTRY(*args, **kwargs):
-    from binsync.decompilers.ida import BinsyncPlugin
+    try:
+        from binsync.decompilers.ida import BinsyncPlugin
+    except ImportError:
+        print("[!] BinSync is not installed, please `pip install binsync` for THIS python interpreter")
+        return None
+
     return BinsyncPlugin(*args, **kwargs)
