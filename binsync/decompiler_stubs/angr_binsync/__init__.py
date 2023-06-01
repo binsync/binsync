22188acc194e6b03950da5bdbb14c42f502a5433
mahaloz <zionbasque@gmail.com>
2023-06-01 10:38:13 -0700

    Update some stubs and versions

--- binsync/decompiler_stubs/angr_binsync/__init__.py
+++ binsync/decompiler_stubs/angr_binsync/__init__.py
@@ -1,4 +1,8 @@
+import sys
+
 try:
     from binsync.decompilers.angr import *
-except ImportError:
-    print("[!] BinSync is not installed, please `pip install binsync` for THIS python interpreter")
+except ImportError as e:
+    print(f"[!] BinSync failed to import something: {e}")
+    print(f"[!] BinSync is probably not installed, please `pip install binsync` for THIS "
+          f"python interpreter: {sys.executable}")
--- binsync/decompiler_stubs/ida_binsync.py
+++ binsync/decompiler_stubs/ida_binsync.py
@@ -1,8 +1,11 @@
+import sys
+
 def PLUGIN_ENTRY(*args, **kwargs):
     try:
         from binsync.decompilers.ida import BinsyncPlugin
-    except ImportError:
-        print("[!] BinSync is not installed, please `pip install binsync` for THIS python interpreter")
-        return None
+    except ImportError as e:
+        print(f"[!] BinSync failed to import something: {e}")
+        print(f"[!] BinSync is probably not installed, please `pip install binsync` for THIS "
+              f"python interpreter: {sys.executable}")
 
     return BinsyncPlugin(*args, **kwargs)
