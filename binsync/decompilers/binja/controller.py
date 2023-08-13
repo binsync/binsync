6a2764379ba5c38087bebb7952d0c060f006e3ca
Saagar Jha <saagar@saagarjha.com>
2023-08-13 01:54:27 -0700

    Fix several type-related mistakes in the Binary Ninja controller (#326)
    * Correctly extract type from parse_type_string
    
    * Build enums correctly
    
    * Handle enums in globals table

--- binsync/decompilers/binja/controller.py
+++ binsync/decompilers/binja/controller.py
@@ -166,7 +166,7 @@ class BinjaBSController(BSController):
                 for offset in sorted(bs_struct.members.keys()):
                     bs_memb = bs_struct.members[offset]
                     try:
-                        bn_type = self.bv.parse_type_string(bs_memb.type) if bs_memb.type else None
+                        bn_type = self.bv.parse_type_string(bs_memb.type)[0] if bs_memb.type else None
                     except Exception:
                         bn_type = None
                     finally:
@@ -184,7 +184,7 @@ class BinjaBSController(BSController):
         changed = False
         bs_global_var: GlobalVariable = artifact
         bn_global_var: binaryninja.DataVariable = self.bv.get_data_var_at(var_addr)
-        global_type = self.bv.parse_type_string(bs_global_var.type)
+        global_type, _ = self.bv.parse_type_string(bs_global_var.type)
         
         if bs_global_var and bs_global_var.name:
             if bn_global_var is None:
@@ -318,7 +318,7 @@ class BinjaBSController(BSController):
         bn_members = []
         
         for member_name, value in bs_enum.members.items():
-            bn_members.append({ "name": member_name, "value": value })
+            bn_members.append((member_name, value))
         
         new_type = binaryninja.TypeBuilder.enumeration(self.bv.arch, bn_members)
         
