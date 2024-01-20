4aa9b21c1f534f7a0780b429aee3203e53089353
mahaloz <zion@zionbasque.com>
2024-01-19 18:52:41 -0700

    IDA and angr working; fixed binja installer

--- binsync/installer.py
+++ binsync/installer.py
@@ -1,3 +1,4 @@
+import shutil
 import textwrap
 from pathlib import Path
 
@@ -11,6 +12,7 @@ class BinSyncInstaller(LibBSPluginInstaller):
         if pkg_files is None:
             raise RuntimeError("Could not find binsync package files. Please reinstall or report on GitHub.")
 
+        self.binsync_files = pkg_files
         self.stub_files = pkg_files / "stub_files"
 
     def display_prologue(self):
@@ -26,7 +28,7 @@ class BinSyncInstaller(LibBSPluginInstaller):
         """))
 
     def _copy_plugin_to_path(self, path):
-        src = self.stub_files / "binsync_plugin.py"
+        src = self.binsync_files / "binsync_plugin.py"
         dst = Path(path) / src.name
         self.link_or_copy(src, dst, symlink=True)
 
@@ -54,5 +56,13 @@ class BinSyncInstaller(LibBSPluginInstaller):
         if not path:
             return
 
-        self._copy_plugin_to_path(path)
+        # binja requires a folder for the plugin
+        bs_binja_dir = path / "binsync"
+        if bs_binja_dir.exists():
+            shutil.rmtree(bs_binja_dir)
+        bs_binja_dir.mkdir()
+
+        # copy things to the new folder
+        self.link_or_copy(self.stub_files / "__init__.py", bs_binja_dir / "__init__.py")
+        self.link_or_copy(self.stub_files / "plugin.json", bs_binja_dir / "plugin.json")
         return path
