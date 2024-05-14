112e2c9196464ba0219fb10822d5398e7cf18a34
mahaloz <zion@zionbasque.com>
2024-05-14 12:09:47 -0400

    Fix AM change

--- binsync/interface_overrides/angr.py
+++ binsync/interface_overrides/angr.py
@@ -27,7 +27,7 @@ class ControlPanelView(BaseView):
     """
 
     def __init__(self, instance, default_docking_position, controller, *args, **kwargs):
-        super().__init__('sync', instance, instance.workspace, default_docking_position, *args, **kwargs)
+        super().__init__('sync', instance.workspace, default_docking_position, *args, **kwargs)
         self.base_caption = "BinSync: Control Panel"
         self.controller: BSController = controller
         self.control_panel = ControlPanel(self.controller)
@@ -73,7 +73,7 @@ class BinsyncPlugin(GenericBSAngrManagementPlugin):
     # BinSync Menu
     #
 
-    MENU_BUTTONS = ('Configure Binsync ...', 'Toggle Binsync Panel')
+    MENU_BUTTONS = ('Start Binsync...', 'Toggle Binsync Panel')
     MENU_CONFIG_ID = 0
     MENU_TOGGLE_PANEL_ID = 1
 
