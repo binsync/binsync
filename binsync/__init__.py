060ad750a289895b00d16cf8f7bc7887982c0d6b
mahaloz <zion@zionbasque.com>
2025-02-11 23:02:57 -0700

    bump

--- binsync/__init__.py
+++ binsync/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "5.2.0"
+__version__ = "5.3.0"
 
 import os
 import platform
@@ -15,8 +15,8 @@ del Loggers
 del logging
 
 # Add /opt/local/bin to PATH for MacOS (gets lost in App launch on most decs)
-#if platform.system() == "Darwin":
-#    os.environ["PATH"] += os.environ["PATH"] + ":/opt/local/bin/"
+if platform.system() == "Darwin":
+    os.environ["PATH"] += os.environ["PATH"] + ":/opt/local/bin/"
 
 
 def create_plugin(*args, **kwargs):
