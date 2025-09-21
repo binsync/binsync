caa105a3795678be0c48d26e263c8bac32751501
justzyen <justzyen@gmail.com>
2025-09-21 10:47:33 -0700

    add a server that does nothing but echo back to a single connection

--- binsync/extras/server/__init__.py
+++ binsync/extras/server/__init__.py
@@ -1,3 +1,13 @@
-def start_server():
+import socket
+def start_server(port=7962):
     print("Starting BinSync server...")
-    # TODO: implement server
+    print("WARNING - ONLY SUPPORTS ONE SINGLE CONNECTION CURRENTLY")
+    server_socket = socket.socket(socket.AF_INET6,socket.SOCK_STREAM) # Can assume that all users will have devices that support ipv6?
+    server_socket.bind(("0:0:0:0:0:0:0:0",port))
+    server_socket.listen()
+    print("Listening to",server_socket.getsockname())
+    conn, addr = server_socket.accept()
+    while True:
+        comm = conn.recv(1024)
+        conn.send(b'Received '+comm)
+    # How to implement server cleanup?
