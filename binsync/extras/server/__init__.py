99d75eec8b9bcbded6f55b722dd5d45b8d98b17a
justzyen <justzyen@gmail.com>
2025-09-28 21:01:16 -0700

    Make server more interesting by allowing it to receive connections and disconnections

--- binsync/extras/server/__init__.py
+++ binsync/extras/server/__init__.py
@@ -1,10 +1,24 @@
 from flask import Flask
-
+from threading import Lock
 app = Flask(__name__)
 
-@app.route('/')
+user_count_lock = Lock()
+user_count = 0
+@app.route('/connect')
 def handle_connection():
-    return 'Hello World'
+    global user_count
+    with user_count_lock:
+        user_count += 1
+        print(user_count)
+    return 'You are connected!'
+
+@app.route("/disconnect")
+def handle_disconnection():
+    global user_count
+    with user_count_lock:
+        user_count -= 1
+        print(user_count)
+    return 'You have disconnected!'
 
 # main driver function
 def start_server(port=7962):
