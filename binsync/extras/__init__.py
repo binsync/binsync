b2e6dc89a2fb429b5588de577390823cd94ce71d
justzyen <justzyen@gmail.com>
2025-09-30 09:42:59 -0700

    Remove "extras are available" message upon running BinSync

--- binsync/extras/__init__.py
+++ binsync/extras/__init__.py
@@ -9,7 +9,6 @@ EXTRAS_AVAILABLE = True
 try:
     # AI Extras
     importlib.import_module("dailalib")
-    _l.info("Extras are available")
 except ImportError:
     _l.info("Extras not installed, some features will not be available.")
     EXTRAS_AVAILABLE = False
