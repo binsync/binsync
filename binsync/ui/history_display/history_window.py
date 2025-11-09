caf4455cc14b5e41d93fad1cf59467dd17ca4951
justzyen <justzyen@gmail.com>
2025-11-09 15:11:02 -0700

    add a table to the history view (currently empty)

--- binsync/ui/history_display/history_window.py
+++ binsync/ui/history_display/history_window.py
@@ -1,21 +1,97 @@
 import logging
 from binsync.controller import BSController
 from datetime import datetime, timezone, timedelta
+from binsync.ui.panel_tabs.table_model import BinsyncTableModel, BinsyncTableView
+
 from libbs.ui.qt_objects import (
     # QtWidgets
     QDialog,
     QHBoxLayout,
     QVBoxLayout,
-    QLabel
+    QLabel,
+    QMenu,
+    QAction,
+    Qt,
+    QAbstractTableModel
 )
 l = logging.getLogger(__name__)
+
+class HistoryTableModel(BinsyncTableModel):
+    def __init__(self, controller: BSController, col_headers=None, filter_cols=None, time_col=None,
+                 addr_col=None, parent=None):
+        super().__init__(controller, col_headers, filter_cols, time_col, addr_col, parent)
+        self.data_dict = {}
+        self.saved_color_window = self.controller.table_coloring_window
+
+        self.saved_ctx = None
+
+    def data(self, index, role=Qt.DisplayRole):
+        if not index.isValid():
+            return None
+
+        col = index.column()
+        row = index.row()
+        if role == Qt.DisplayRole:
+            return self.row_data[row][col]
+        elif role == self.SortRole:
+            return self.row_data[row][col]
+        elif role == Qt.BackgroundRole:
+            return self.data_bgcolors[row]
+        elif role == self.FilterRole:
+            return self.row_data[row][0] + " " + self.row_data[row][1]
+        elif role == Qt.ToolTipRole:
+            #return self.data_tooltips[row]
+            pass
+        return None
+
+    # No update_table because we don't care about the current state
+
+class HistoryTableView(BinsyncTableView):
+    HEADER = ['Addr', 'Function']
+
+    def __init__(self, controller: BSController, stretch_col=None,
+                 col_count=None, parent=None):
+        super().__init__(controller, None, 1, 2, parent)
+
+        self.model = HistoryTableModel(controller, self.HEADER, addr_col=0,
+                                        parent=parent)
+        self.proxymodel.setSourceModel(self.model)
+        self.setModel(self.proxymodel)
+
+        # always init settings *after* loading the model
+        self._init_settings()
+
+    def contextMenuEvent(self, event):
+        menu = QMenu(self)
+        menu.setObjectName("binsync_history_table_context_menu")
+
+        valid_row = True
+        selected_row = self.rowAt(event.pos().y())
+        idx = self.proxymodel.index(selected_row, 0)
+        idx = self.proxymodel.mapToSource(idx)
+        if event.pos().y() == -1 and event.pos().x() == -1:
+            idx = self.proxymodel.index(0, 0)
+            idx = self.proxymodel.mapToSource(idx)
+        elif not (0 <= selected_row < len(self.model.row_data)) or not idx.isValid():
+            valid_row = False
+
+        col_hide_menu = menu.addMenu("Show Columns")
+        handler = lambda ind: lambda: self._col_hide_handler(ind)
+        for i, c in enumerate(self.HEADER):
+            act = QAction(c, parent=menu)
+            act.setCheckable(True)
+            act.setChecked(self.column_visibility[i])
+            act.triggered.connect(handler(i))
+            col_hide_menu.addAction(act)
+
+        menu.popup(self.mapToGlobal(event.pos()))
 
 class HistoryDisplayWidget(QDialog):
     def __init__(self,controller:BSController=None,parent=None):
         super().__init__(parent)
         self.controller = controller
         self._init_widgets()
-        self._calculate_diff()
+        self._display_diff()
         
     def _init_widgets(self):
         self.setWindowTitle("History")
@@ -25,7 +101,8 @@ class HistoryDisplayWidget(QDialog):
         bottom_layout = QVBoxLayout()
         
         top_layout.addWidget(QLabel("top"))
-        bottom_layout.addWidget(QLabel("bottom"))
+        self.table_view = HistoryTableView(self.controller)
+        bottom_layout.addWidget(self.table_view)
         
         
         main_layout.addLayout(top_layout)
@@ -34,7 +111,7 @@ class HistoryDisplayWidget(QDialog):
         self.setLayout(main_layout)
         self.resize(1000, 800)
         
-    def _calculate_diff(self):
+    def _display_diff(self):
         changed_functions = []
         client = self.controller.client
         previous_time =  (datetime.now(timezone.utc)-timedelta(days=1)).timestamp()
@@ -54,6 +131,7 @@ class HistoryDisplayWidget(QDialog):
                         break
         for function in changed_functions:
             l.info(function)
+            self.table_view.model.insertRows(0)
     
     def _get_function_diffs(self,state1, state2, addr)->dict[str,dict[str,any]]:
         '''
