74b1b52c7a647bceb282272dfa69e569bdfc6913
justzyen <justzyen@gmail.com>
2025-11-10 17:27:35 -0700

    Add test to verify the server doesn't crash instantly

--- tests/test_auxiliary_server.py
+++ tests/test_auxiliary_server.py
@@ -10,6 +10,25 @@ from binsync.controller import BSController
 class TestAuxServer(unittest.TestCase):
     HOST = "::"
     PORT = 7962
+    
+    def test_server_no_crash(self):
+        """
+        Make sure that the server can start up without issues.
+        """
+        def server_task(server:Server,crash_signal:threading.Event):
+            try:
+                server.run()
+            except:
+                crash_signal.set()
+        server = Server(self.HOST,self.PORT)
+        crash_signal = threading.Event()
+        # Make server thread as a daemon
+        server_thread = threading.Thread(target=server_task,args=(server,crash_signal))
+        server_thread.daemon = True
+        server_thread.start()
+        server_thread.join(1)
+        self.assertFalse(crash_signal.is_set())
+        
     def test_run_server(self):
         def server_task(server:Server):
             server.run()
@@ -20,6 +39,8 @@ class TestAuxServer(unittest.TestCase):
         server_thread.start()
         time.sleep(1)
         self.assertEqual(server.store._user_map,{})
+        
+
 
 
 
