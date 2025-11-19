e01504fc6c8953785d4414f479a0c64f85a3d11e
lukealpe <lukeperez@gmail.com>
2025-11-18 23:07:07 -0700

    Changed model to gpt-5 for real

--- binsync/ui/progress_graph/summarize.py
+++ binsync/ui/progress_graph/summarize.py
@@ -65,7 +65,7 @@ def summarize_changes(controller: "BSController", graph: nx.DiGraph, save_locati
         decompilation_text += decompilations[func].text + "\n\n"
 
     total_text = PRE_TEXT + decompilation_text + POST_TEXT
-    thread = threading.Thread(target=query_model, args=("gpt-4", total_text, save_location), daemon=True)
+    thread = threading.Thread(target=query_model, args=("gpt-5", total_text, save_location), daemon=True)
     thread.start()
 
 
